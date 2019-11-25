8ea1c1d5cc2eacba2bacf427d02475dbd19c85af
Teodor Trifunov <teodor.trifunov@nuvoxx.com>
2019-11-25 14:48:30 +0100

    Configure routing.

---
+++ PuzzleBase.Web/CodeBehind/PuzzleBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PuzzleBase.Web.CodeBehind
+{
+    public class PuzzleBase : ComponentBase
+    {
+        [Parameter]
+        public int ID { get; set; }
+    }
+}
