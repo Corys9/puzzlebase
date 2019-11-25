da262b5ab6da6b5d0a44cd9f832ebb752ce9bb09
Teodor Trifunov <teodor.trifunov@nuvoxx.com>
2019-11-25 12:26:07 +0100

    Create component library. Turn off API page during debugging.

---
+++ PuzzleBase.Components/CodeBehind/PuzzleTileBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using PuzzleBase.Models;
+
+namespace PuzzleBase.Components.CodeBehind
+{
+    public class PuzzleTileBase : ComponentBase
+    {
+        [Parameter]
+        public Puzzle Puzzle { get; set; }
+    }
+}
